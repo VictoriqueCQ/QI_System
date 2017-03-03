90d86435afebd4ec7bf22be5d7ad5dcf0ef5905d
returncyy <1226550411@qq.com>
2017-03-03 16:29:30 +0800

    com

---
+++ presentation/src/main/java/ui/CompareFunction.java
@@ -0,0 +1,18 @@
+package ui;/**
+ * Created by chenyuyan on 17/3/3.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CompareFunction extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
