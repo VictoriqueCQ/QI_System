c12213cc68cf3669ea001aff7b401aaf976e41a6
151250171 <151250171@smail.nju.edu.cn>
2017-03-03 16:06:42 +0800

    test

---
+++ presentation/src/main/java/ui/Main.java
@@ -0,0 +1,70 @@
+package ui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    // 主窗口
+    private Stage stage;
+
+
+
+    // 内部窗口
+    private SplitPane rootLayout;
+
+    private Scene scene;
+    private final double MINIMUM_WINDOW_WIDTH = 400.0;
+    private final double MINIMUM_WINDOW_HEIGHT = 250.0;
+
+    /**
+     * 跳转到客户界面
+     */
+    public void gotoClientOverview() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(Main.class.getResource("/ClientOverview.fxml"));
+            rootLayout = (SplitPane) fxmlLoader.load();
+            rootLayout.setPrefSize(1000, 600);
+            rootLayout.setDividerPositions(0.3f);
+            ClientOverviewController controller = (ClientOverviewController) fxmlLoader.getController();
+            controller.setMain(this);
+            Scene scene = new Scene(rootLayout);
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.show();
+        } catch (Exception e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        stage.initStyle(StageStyle.UNDECORATED);
+//		stage.setTitle("Hotel");
+        stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+        stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+        stage.setResizable(false);
+        this.gotoClientOverview();
+    }
+
+    /**
+     * 退出系统
+     */
+    public void exitSystem() {
+        stage.close();
+    }
+
+}
+
