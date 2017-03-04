580a1dea0024b0d3f1c48c77ec359c21d495ac82
iriswuyuhan <1426546185@qq.com>
2017-03-04 19:15:26 +0800

    update file structure and add interfaces and vos

--- common/src/main/java/vo/StockVO.java
+++ common/src/main/java/vo/StockVO.java
@@ -1,50 +1,63 @@
 package vo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * 单只股票VO
  * Created by dell on 2017/3/4.
  */
 public class StockVO {
-    private ArrayList<Double> open;
-    private ArrayList<Double> close;
-    private ArrayList<Double> high;
-    private ArrayList<Double> low;
-    private ArrayList<Double> average;
-    private ArrayList<Double> variance;//相对方差
+    private Iterator open;
+    private Iterator close;
+    private Iterator rehabClose;//复权后
+    private Iterator volume;//交易数
+    private Iterator high;
+    private Iterator low;
+    private Iterator average;
+    private Iterator variance;//相对方差
 
-    public StockVO(ArrayList<Double> open, ArrayList<Double> close, ArrayList<Double> high, ArrayList<Double> low,
-                   ArrayList<Double> average, ArrayList<Double> variance) {
+    public StockVO(Iterator open, Iterator close, Iterator rehabClose, Iterator volume, Iterator high, Iterator low,
+                   Iterator average, Iterator variance) {
         this.open = open;
         this.close = close;
+        this.rehabClose = rehabClose;
+        this.volume = volume;
         this.high = high;
         this.low = low;
         this.average = average;
         this.variance = variance;
     }
 
-    public ArrayList<Double> getOpen() {
+    public Iterator getOpen() {
         return open;
     }
 
-    public ArrayList<Double> getClose() {
+    public Iterator getClose() {
         return close;
     }
 
-    public ArrayList<Double> getHigh() {
+    public Iterator getRehabClose() {
+        return rehabClose;
+    }
+
+    public Iterator getVolume() {
+        return volume;
+    }
+
+    public Iterator getHigh() {
         return high;
     }
 
-    public ArrayList<Double> getLow() {
+    public Iterator getLow() {
         return low;
     }
 
-    public ArrayList<Double> getAverage() {
+    public Iterator getAverage() {
         return average;
     }
 
-    public ArrayList<Double> getVariance() {
+    public Iterator getVariance() {
         return variance;
     }
 }
