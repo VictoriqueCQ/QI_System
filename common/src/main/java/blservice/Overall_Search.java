115ea45c0b2ef417f4b960e36c7ad615ccf8064e
iriswuyuhan <1426546185@qq.com>
2017-03-04 17:25:07 +0800

    update file structure and add interfaces and vos

---
+++ common/src/main/java/blservice/Overall_Search.java
@@ -0,0 +1,8 @@
+package blservice;
+
+/**
+ * 用于整体市场搜索
+ * Created by dell on 2017/3/4.
+ */
+public interface Overall_Search {
+}
---
+++ common/src/main/java/vo/MarketSearchConditionVO.java
@@ -0,0 +1,7 @@
+package vo;
+
+/**
+ * Created by dell on 2017/3/4.
+ */
+public class MarketSearchConditionVO {
+}
