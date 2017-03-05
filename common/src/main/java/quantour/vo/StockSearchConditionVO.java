14e2af2fd309db2c7dfd92b76db35a5fa79668a3
iriswuyuhan <1426546185@qq.com>
2017-03-05 15:57:59 +0800

    update file structure and add interfaces and vos

--- common/src/main/java/quantour/vo/StockSearchConditionVO.java
+++ common/src/main/java/quantour/vo/StockSearchConditionVO.java
@@ -1,4 +1,4 @@
-package vo;
+package quantour.vo;
 
 import java.util.Date;
 
---
+++ server/src/main/java/quantour/dataservice/DataFactory.java
@@ -0,0 +1,9 @@
+package quantour.dataservice;
+
+/**
+ * Created by dell on 2017/3/4.
+ */
+public interface DataFactory {
+    Overall_Search_data getOverallSearch();
+    Single_Search_data getSingleSearch();
+}
