32c481ca4c6fe8b20a15610c5e4e3b2bc5a32fe0
iriswuyuhan <1426546185@qq.com>
2017-03-05 17:11:27 +0800

    update interfaces and blImpl and dataImpl

--- server/src/main/java/quantour/data/Single_Search_data_Impl.java
+++ server/src/main/java/quantour/data/Single_Search_data_Impl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 /**
  * Created by dell on 2017/3/4.
  */
-public class Single_Search_CVS_Impl implements Single_Search_data{
+public class Single_Search_data_Impl implements Single_Search_data{
     @Override
     public ArrayList<StockPO> getStockList() {
         return null;
