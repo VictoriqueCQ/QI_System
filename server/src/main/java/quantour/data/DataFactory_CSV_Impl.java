d3b175f0087fb22281a19037212773e854eef0c5
151250162 <151250162@smail.nju.edu.cn>
2017-03-05 19:37:37 +0800

    read data

--- server/src/main/java/quantour/data/DataFactory_CSV_Impl.java
+++ server/src/main/java/quantour/data/DataFactory_CSV_Impl.java
@@ -3,9 +3,9 @@ package quantour.data;
 import quantour.dataservice.DataFactory;
 import quantour.dataservice.Overall_Search_data;
 import quantour.dataservice.Single_Search_data;
-import quantour.po.StockPO;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dell on 2017/3/4.
@@ -16,7 +16,7 @@ public class DataFactory_CSV_Impl implements DataFactory{
     private Overall_Search_data overallSearchData;
     private Single_Search_data singleSearchData;
 
-    private List<StockPO> stockPOList;
+    private Map<StockIdentifier,List> stockPOList;
 
     private DataFactory_CSV_Impl(){
         DataReader_CSV dataReader_CSV =new DataReader_CSV();
--- server/src/main/java/quantour/data/DataReader_CSV.java
+++ server/src/main/java/quantour/data/DataReader_CSV.java
@@ -6,8 +6,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 /**
@@ -24,23 +25,63 @@ public class DataReader_CSV {
         this.path=path;
     }
 
-    List<StockPO> read(){
+    Map<StockIdentifier,List> read(){
         List<StockPO> stockPOList=new ArrayList<>();
         File f=new File(path);
 
+        Map<StockIdentifier,List> stockMap=new HashMap<>();
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/mm/yy");
         try{
             BufferedReader br=new BufferedReader(new FileReader(f));
             br.readLine();//标题行
-            String row;
-            while((row=br.readLine().trim())!=null){
-                String[] stockInfo=row.split("\t");
+            String row=br.readLine();
+            while(row!=null){
+                List<String> stockInfo=Arrays.asList(row.split("\t"));
+                int serial=Integer.parseInt(stockInfo.get(0));
+                List<StockDealInfo> stockDealInfos=new ArrayList<>();
 
+                //处理stockIdentifier
+                String code=stockInfo.get(8);
+                String name=stockInfo.get(9);
+                String market=stockInfo.get(10);
+                StockIdentifier stockIdentifier=new StockIdentifier(code,name,market);
+
+                row=br.readLine();
+                if(row!=null){
+                    stockInfo=Arrays.asList(row.split("\t"));
+                    serial=Integer.parseInt(stockInfo.get(0));
+                }
+                while(row!=null&&serial!=0) {
+                    //处理stockDealInfo
+                    Date date = sdf.parse(stockInfo.get(1));
+                    double open = Double.parseDouble(stockInfo.get(2));
+                    double high = Double.parseDouble(stockInfo.get(3));
+                    double low = Double.parseDouble(stockInfo.get(4));
+                    double close = Double.parseDouble(stockInfo.get(5));
+                    int volume = Integer.parseInt(stockInfo.get(6));
+                    double adjClose = Double.parseDouble(stockInfo.get(7));
+                    StockDealInfo stockDealInfo = new StockDealInfo(serial, date, open, high, low, close, volume, adjClose);
+                    stockDealInfos.add(stockDealInfo);
+
+                    row=br.readLine();
+                    if(row!=null){
+                        stockInfo=Arrays.asList(row.split("\t"));
+                        serial=Integer.parseInt(stockInfo.get(0));
+                    }
+                }
+
+                stockMap.put(stockIdentifier,stockDealInfos);
             }
+
+            br.close();
         }catch (IOException ioe){
             ioe.printStackTrace();
+        }catch (ParseException pe) {
+            pe.printStackTrace();
         }
 
-        return stockPOList;
+        return stockMap;
     }
+
 
 }
