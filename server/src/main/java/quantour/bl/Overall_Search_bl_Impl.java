db2165aae1adf2bf99ba221667d9e719811ea7a7
iriswuyuhan <1426546185@qq.com>
2017-03-05 15:57:59 +0800

    update file structure and add interfaces and vos

---
+++ server/src/main/java/quantour/bl/Overall_Search_bl_Impl.java
@@ -0,0 +1,17 @@
+package quantour.bl;
+
+
+import quantour.blservice.Overall_Search_bl;
+import quantour.vo.MarketVO;
+
+import java.util.Date;
+
+/**
+ * Created by dell on 2017/3/4.
+ */
+public class Overall_Search_bl_Impl implements Overall_Search_bl{
+    @Override
+    public MarketVO getMarketInfo(Date date) {
+        return null;
+    }
+}
