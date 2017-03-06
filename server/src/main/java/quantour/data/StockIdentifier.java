eebe69927040475a784ff2b7d51d40f1d45ba87a
151250162 <151250162@smail.nju.edu.cn>
2017-03-06 19:12:30 +0800

    read data

--- server/src/main/java/quantour/data/StockIdentifier.java
+++ server/src/main/java/quantour/data/StockIdentifier.java
@@ -4,11 +4,11 @@ package quantour.data;
  * Created by dell on 2017/3/5.
  */
 public class StockIdentifier {
-    private String name;
     private String code;
+    private String name;
     private String market;
 
-    StockIdentifier(String name, String code, String market) {
+    StockIdentifier( String code,String name, String market) {
         this.name = name;
         this.code = code;
         this.market = market;
