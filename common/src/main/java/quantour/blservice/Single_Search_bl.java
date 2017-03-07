dff8eb04bcb2f3caa80a8b51cd465ba89c1975fd
151250020 <151250020@smail.nju.edu.cn>
2017-03-07 23:42:25 +0800

    股票比较界面修改

--- common/src/main/java/quantour/blservice/Single_Search_bl.java
+++ common/src/main/java/quantour/blservice/Single_Search_bl.java
@@ -1,5 +1,6 @@
 package quantour.blservice;
 
+import quantour.vo.ContrastVO;
 import quantour.vo.StockSearchConditionVO;
 import quantour.vo.StockVO;
 
@@ -9,4 +10,13 @@ import quantour.vo.StockVO;
  */
 public interface Single_Search_bl {
     StockVO findStock(StockSearchConditionVO stockSearchConditionVO);
+
+    /**
+     * 用于计算两支股票比较数据
+     * @param searchConditionVO
+     * @return
+     * Created by cyy on 2017/3/7
+     */
+    public ContrastVO contrastInfo(StockSearchConditionVO searchConditionVO);
+
 }
