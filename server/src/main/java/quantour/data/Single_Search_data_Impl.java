2fe57686d93eed9c9f2625f3d82f6cde2537c107
151250162 <151250162@smail.nju.edu.cn>
2017-03-09 19:12:55 +0800

    read data

--- server/src/main/java/quantour/data/Single_Search_data_Impl.java
+++ server/src/main/java/quantour/data/Single_Search_data_Impl.java
@@ -2,15 +2,34 @@ package quantour.data;
 
 import quantour.dataservice.Single_Search_data;
 import quantour.po.StockPO;
+import quantour.vo.StockVO;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by dell on 2017/3/4.
  */
 public class Single_Search_data_Impl implements Single_Search_data{
+    private List<Stock> stockList;
+
+    Single_Search_data_Impl(List<Stock> stockList){
+        this.stockList =stockList;
+    }
+
     @Override
-    public ArrayList<StockPO> getStockList() {
+    public ArrayList<StockPO> getStockListByID(int stockID, Date startTime, Date endTime) {
+        List<Stock> singleStockList=stockList.stream().
+                filter(stock -> ((stock.getCode()==stockID)&&(stock.getDate().after(startTime))&&(stock.getDate().before(endTime)))).
+                collect(Collectors.toList());
+
+        return null;
+    }
+
+    @Override
+    public ArrayList<StockVO> getStockListByName(String stockName, Date startTime, Date endTime) {
         return null;
     }
 }
