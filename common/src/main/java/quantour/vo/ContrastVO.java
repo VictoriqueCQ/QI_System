33e2307bbdc1fc19efe7b41c475f51815d1aa8a2
iriswuyuhan <1426546185@qq.com>
2017-03-09 19:12:55 +0800

    read data

--- common/src/main/java/quantour/vo/ContrastVO.java
+++
@@ -1,7 +0,0 @@
-package quantour.vo;
-
-/**
- * Created by chenyuyan on 7/3/17.
- */
-public class ContrastVO {
-}
--- server/src/main/java/quantour/data/Overall_Search_data_Impl.java
+++ server/src/main/java/quantour/data/Overall_Search_data_Impl.java
@@ -3,14 +3,27 @@ package quantour.data;
 import quantour.dataservice.Overall_Search_data;
 import quantour.po.MarketPO;
 
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by dell on 2017/3/4.
  */
 public class Overall_Search_data_Impl implements Overall_Search_data{
+    private List<Stock> marketMap;
+
+    Overall_Search_data_Impl(List<Stock> stockList) {
+        this.marketMap=stockList;
+    }
+
     @Override
     public MarketPO getMarketInfo(Date date) {
+        List<Stock> today=marketMap.parallelStream().filter(stock -> stock.getDate().compareTo(date)==0).
+                sorted(Comparator.comparing(Stock::getCode)).collect(Collectors.toList());
+        long sum=today.parallelStream().mapToInt(value -> value.getVolume()).reduce(0,(x,y)->x+y);
+
         return null;
     }
 }
