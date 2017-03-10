3c04a9b1602eea55aedaa8064c6e4521662eec65
151250162 <151250162@smail.nju.edu.cn>
2017-03-10 15:46:35 +0800

    single stock operations（not completed yet）

--- server/src/main/java/quantour/dataservice/Single_Search_data.java
+++ server/src/main/java/quantour/dataservice/Single_Search_data.java
@@ -1,15 +1,13 @@
 package quantour.dataservice;
 
 import quantour.po.StockPO;
-import quantour.vo.StockVO;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * Created by dell on 2017/3/4.
  */
 public interface Single_Search_data {
-    ArrayList<StockPO> getStockListByID(int stockID, Date startTime,Date endTime);
-    ArrayList<StockVO> getStockListByName(String stockName,Date startTime,Date endTime);
+    StockPO getStockListByID(int stockID, Date startTime,Date endTime);
+    StockPO getStockListByName(String stockName,Date startTime,Date endTime);
 }
