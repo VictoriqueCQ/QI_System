e3c2c181c69bb565f66b609f8d6765942eee6abf
xjwhhh <920054996@qq.com>
2017-03-10 21:59:36 +0800

    logo，头像

--- presentation/src/main/java/ui/CandlestickChartController.java
+++ presentation/src/main/java/ui/CandlestickChartController.java
@@ -94,9 +94,13 @@ public class CandlestickChartController {
         endTimeDatePicker.setDayCellFactory(dayCellFactory1);
     }
 
+    XYChart.Series series11 = new XYChart.Series();
+    XYChart.Series series22= new XYChart.Series();
+    XYChart.Series series33 = new XYChart.Series();
     @FXML
     private void search(){
         this.createCandlestickChart();
+
         this.createEMA();
     }
 
@@ -299,60 +303,62 @@ public class CandlestickChartController {
     //public OHLCSeries addData(OHLCSeries series,)
 
     private void createEMA(){
-//        lineChart.setBackground(new Background());
+//        lineChart.getData().removeAll(series11,series22,series33);
         final CategoryAxis xAxis = new CategoryAxis();
         final javafx.scene.chart.NumberAxis yAxis = new javafx.scene.chart.NumberAxis();
         xAxis.setLabel("Month");
 
+
         lineChart.setTitle("Stock Monitoring, 2010");
 
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Portfolio 1");
+//        XYChart.Series series1 = new XYChart.Series();
+        series11.setName("Portfolio 1");
 
-        series1.getData().add(new XYChart.Data("Jan", 23));
-        series1.getData().add(new XYChart.Data("Feb", 14));
-        series1.getData().add(new XYChart.Data("Mar", 15));
-        series1.getData().add(new XYChart.Data("Apr", 24));
-        series1.getData().add(new XYChart.Data("May", 34));
-        series1.getData().add(new XYChart.Data("Jun", 36));
-        series1.getData().add(new XYChart.Data("Jul", 22));
-        series1.getData().add(new XYChart.Data("Aug", 45));
-        series1.getData().add(new XYChart.Data("Sep", 43));
-        series1.getData().add(new XYChart.Data("Oct", 17));
-        series1.getData().add(new XYChart.Data("Nov", 29));
-        series1.getData().add(new XYChart.Data("Dec", 25));
+        series11.getData().add(new XYChart.Data("Jan", 23));
+        series11.getData().add(new XYChart.Data("Feb", 14));
+        series11.getData().add(new XYChart.Data("Mar", 15));
+        series11.getData().add(new XYChart.Data("Apr", 24));
+        series11.getData().add(new XYChart.Data("May", 34));
+        series11.getData().add(new XYChart.Data("Jun", 36));
+        series11.getData().add(new XYChart.Data("Jul", 22));
+        series11.getData().add(new XYChart.Data("Aug", 45));
+        series11.getData().add(new XYChart.Data("Sep", 43));
+        series11.getData().add(new XYChart.Data("Oct", 17));
+        series11.getData().add(new XYChart.Data("Nov", 29));
+        series11.getData().add(new XYChart.Data("Dec", 25));
 
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("Portfolio 2");
-        series2.getData().add(new XYChart.Data("Jan", 33));
-        series2.getData().add(new XYChart.Data("Feb", 34));
-        series2.getData().add(new XYChart.Data("Mar", 25));
-        series2.getData().add(new XYChart.Data("Apr", 44));
-        series2.getData().add(new XYChart.Data("May", 39));
-        series2.getData().add(new XYChart.Data("Jun", 16));
-        series2.getData().add(new XYChart.Data("Jul", 55));
-        series2.getData().add(new XYChart.Data("Aug", 54));
-        series2.getData().add(new XYChart.Data("Sep", 48));
-        series2.getData().add(new XYChart.Data("Oct", 27));
-        series2.getData().add(new XYChart.Data("Nov", 37));
-        series2.getData().add(new XYChart.Data("Dec", 29));
+//        XYChart.Series series2 = new XYChart.Series();
+        series22.setName("Portfolio 2");
+        series22.getData().add(new XYChart.Data("Jan", 33));
+        series22.getData().add(new XYChart.Data("Feb", 34));
+        series22.getData().add(new XYChart.Data("Mar", 25));
+        series22.getData().add(new XYChart.Data("Apr", 44));
+        series22.getData().add(new XYChart.Data("May", 39));
+        series22.getData().add(new XYChart.Data("Jun", 16));
+        series22.getData().add(new XYChart.Data("Jul", 55));
+        series22.getData().add(new XYChart.Data("Aug", 54));
+        series22.getData().add(new XYChart.Data("Sep", 48));
+        series22.getData().add(new XYChart.Data("Oct", 27));
+        series22.getData().add(new XYChart.Data("Nov", 37));
+        series22.getData().add(new XYChart.Data("Dec", 29));
 
-        XYChart.Series series3 = new XYChart.Series();
-        series3.setName("Portfolio 3");
-        series3.getData().add(new XYChart.Data("Jan", 44));
-        series3.getData().add(new XYChart.Data("Feb", 35));
-        series3.getData().add(new XYChart.Data("Mar", 36));
-        series3.getData().add(new XYChart.Data("Apr", 33));
-        series3.getData().add(new XYChart.Data("May", 31));
-        series3.getData().add(new XYChart.Data("Jun", 26));
-        series3.getData().add(new XYChart.Data("Jul", 22));
-        series3.getData().add(new XYChart.Data("Aug", 25));
-        series3.getData().add(new XYChart.Data("Sep", 43));
-        series3.getData().add(new XYChart.Data("Oct", 44));
-        series3.getData().add(new XYChart.Data("Nov", 45));
-        series3.getData().add(new XYChart.Data("Dec", 44));
+//        XYChart.Series series3 = new XYChart.Series();
+        series33.setName("Portfolio 3");
+        series33.getData().add(new XYChart.Data("Jan", 44));
+        series33.getData().add(new XYChart.Data("Feb", 35));
+        series33.getData().add(new XYChart.Data("Mar", 36));
+        series33.getData().add(new XYChart.Data("Apr", 33));
+        series33.getData().add(new XYChart.Data("May", 31));
+        series33.getData().add(new XYChart.Data("Jun", 26));
+        series33.getData().add(new XYChart.Data("Jul", 22));
+        series33.getData().add(new XYChart.Data("Aug", 25));
+        series33.getData().add(new XYChart.Data("Sep", 43));
+        series33.getData().add(new XYChart.Data("Oct", 44));
+        series33.getData().add(new XYChart.Data("Nov", 45));
+        series33.getData().add(new XYChart.Data("Dec", 44));
 
-        lineChart.getData().addAll(series1, series2, series3);
+        lineChart.getData().addAll(series11, series22, series33);
+        gridPane.getChildren().removeAll(lineChart);
         gridPane.add(lineChart,1,0);
 
     }
