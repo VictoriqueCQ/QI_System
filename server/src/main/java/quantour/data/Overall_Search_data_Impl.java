12a1644bce26decdc4644670da588d31f4fc5af0
iriswuyuhan <1426546185@qq.com>
2017-03-11 17:07:27 +0800

    market stock operations(not completed yet)

--- server/src/main/java/quantour/data/Overall_Search_data_Impl.java
+++ server/src/main/java/quantour/data/Overall_Search_data_Impl.java
@@ -31,17 +31,21 @@ public class Overall_Search_data_Impl implements Overall_Search_data{
         long sum=today.parallelStream().mapToInt(Stock::getVolume).reduce(0,(x, y)->x+y);
 
         //计算涨停和跌停的
+        int limitUpNum= countNumOfIncreasing(today,0.1);
+        int limitDownNum= countNumOfDecresing(today,-0.1);
 
         //计算涨幅超过5%的股票数和跌幅超过5%的股票数
+        int overFivePerNum=countNumOfIncreasing(today,0.05);
+        int belowFivePerNum=countNumOfDecresing(today,-0.05);
 
         //计算开盘-收盘大于5%*上一个交易日收盘价的股票个数和开盘-收盘小于-5%*上一个交易日收盘价的股票个数
-        long oc_overPFivePerNum= today.parallelStream().
+        int oc_overPFivePerNum= (int) today.parallelStream().
                 filter(stock -> {
                     Stock previous=marketList.get(marketList.indexOf(stock)+1);
                     return previous.getSerial()!=0&&stock.getClose()-stock.getOpen()>0.05*previous.getClose();
                 }).
                 count();
-        long oc_belowMFivePerNum=today.parallelStream().
+        int oc_belowMFivePerNum= (int) today.parallelStream().
                 filter(stock -> {
                     Stock previous=marketList.get(marketList.indexOf(stock)+1);
                     return previous.getSerial()!=0&&stock.getClose()-stock.getOpen()<-0.05*previous.getClose();
@@ -49,17 +53,27 @@ public class Overall_Search_data_Impl implements Overall_Search_data{
                 count();
 
 
-        return null;
+        return new MarketPO(sum,limitUpNum,limitDownNum,overFivePerNum,belowFivePerNum,oc_overPFivePerNum,oc_belowMFivePerNum);
     }
 
-    private int countNumOfStock(List<Stock> today,double percentage){
+    private int countNumOfIncreasing(List<Stock> today,double percentage){
 
-        return 0;
+        return (int) today.parallelStream().
+                filter(stock -> {
+                    Stock previous=marketList.get(marketList.indexOf(stock)+1);
+                    return previous.getSerial()!=0&&stock.getClose()-previous.getClose()>=percentage;
+                }).
+                count();
     }
 
-    private int countOCNumOfStock(List<Stock> today,double percentage){
+    private int countNumOfDecresing(List<Stock> today,double percentage){
 
-        return 0;
+        return (int) today.parallelStream().
+                filter(stock -> {
+                    Stock previous=marketList.get(marketList.indexOf(stock)+1);
+                    return previous.getSerial()!=0&&stock.getClose()-previous.getClose()<=percentage;
+                }).
+                count();
     }
 
 }
