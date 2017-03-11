6bcd7417ed3d55ad6465da19764c4e0a65da9353
151250162 <151250162@smail.nju.edu.cn>
2017-03-11 21:04:20 +0800

    some

---
+++ server/src/main/java/quantour/bl/DataNodeThread.java
@@ -0,0 +1,11 @@
+package quantour.bl;
+
+/**
+ * Created by dell on 2017/3/11.
+ */
+public class DataNodeThread implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
---
+++ server/src/main/java/quantour/bl/Main.java
@@ -0,0 +1,11 @@
+package quantour.bl;
+
+/**
+ * Created by dell on 2017/3/11.
+ */
+public class Main {
+    public static void main(String[] args) {
+        SwitchNode switchNode=new SwitchNode();
+        switchNode.run();
+    }
+}
