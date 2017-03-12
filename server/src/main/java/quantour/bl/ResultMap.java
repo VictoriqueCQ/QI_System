a4e478c31fb95eb7e595511dab5ee01bbabdf331
151250162 <151250162@smail.nju.edu.cn>
2017-03-12 15:05:00 +0800

    socket(not completed yet)

---
+++ server/src/main/java/quantour/bl/ResultMap.java
@@ -0,0 +1,19 @@
+package quantour.bl;
+
+import java.util.Map;
+
+/**
+ * Created by dell on 2017/3/12.
+ */
+public class ResultMap {
+    private static Map<Integer,String> map;
+
+    private static void write(){
+
+    }
+
+    private static void get(){
+
+    }
+
+}
