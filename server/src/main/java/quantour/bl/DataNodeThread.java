97ee4341e61fd28fe89afc6b9ef4ac0370250c32
151250162 <151250162@smail.nju.edu.cn>
2017-03-12 16:58:08 +0800

    switching socket

--- server/src/main/java/quantour/bl/DataNodeThread.java
+++ server/src/main/java/quantour/bl/DataNodeThread.java
@@ -1,5 +1,6 @@
 package quantour.bl;
 
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -7,14 +8,41 @@ import java.net.Socket;
  */
 public class DataNodeThread implements Runnable{
     private Socket socket;
+    private BufferedReader br;
+    private PrintWriter printWriter;
 
     DataNodeThread(Socket socket){
         this.socket=socket;
     }
 
-
     @Override
     public void run() {
-        
+        try {
+            InputStreamReader inputStreamReader=new InputStreamReader(socket.getInputStream());
+            br=new BufferedReader(inputStreamReader);
+            printWriter=new PrintWriter(socket.getOutputStream());
+
+            String quest=null;
+            while (socket.isConnected()){
+                if ((quest=QuestQue.get())==null){
+                    continue;
+                }
+                String[] readKey=quest.split("\t");
+                int key=Integer.parseInt(readKey[0]);
+                printWriter.write(quest);
+                printWriter.flush();
+                Thread.sleep(100);
+
+                String result=null;
+                while((result=br.readLine())==null){
+                    Thread.sleep(100);
+                }
+                ResultMap.write(key,result);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
