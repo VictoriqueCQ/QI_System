892a7f99e378cb71e0548dbdcea602f33f909a5d
151250162 <151250162@smail.nju.edu.cn>
2017-03-12 15:05:00 +0800

    socket(not completed yet)

---
+++ server/src/main/java/quantour/bl/QuestQue.java
@@ -0,0 +1,18 @@
+package quantour.bl;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by dell on 2017/3/12.
+ */
+public class QuestQue {
+    private static LinkedBlockingQueue<String> queue;
+
+    public static void write(String quest){
+
+    }
+
+    public static void get(String quest){
+
+    }
+}
