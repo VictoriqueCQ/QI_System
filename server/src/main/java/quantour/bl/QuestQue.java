c18cf4352a187ade0da2d2b499260479d1c5585e
151250162 <151250162@smail.nju.edu.cn>
2017-03-12 16:58:08 +0800

    switching socket

--- server/src/main/java/quantour/bl/QuestQue.java
+++ server/src/main/java/quantour/bl/QuestQue.java
@@ -5,14 +5,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 /**
  * Created by dell on 2017/3/12.
  */
-public class QuestQue {
-    private static LinkedBlockingQueue<String> queue;
+class QuestQue {
+    private static LinkedBlockingQueue<String> queue=new LinkedBlockingQueue<>();
+    private static int id=0;
 
-    public static void write(String quest){
-
+    static int write(String quest) throws InterruptedException {
+        queue.put(id+"\t"+quest);
+        return id++;//目前只能叠加，应该在一段时间后进行清零
     }
 
-    public static void get(String quest){
-
+    static String get(){
+        return queue.poll();
     }
 }
