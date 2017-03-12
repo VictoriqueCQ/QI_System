9721bf1aa0bf3f208c5f5f35358d148125c650d8
xjwhhh <920054996@qq.com>
2017-03-12 20:25:22 +0800

    文件结构；提示面板；socket不知道改的对不对

---
+++ presentation/src/main/java/ui/AlertUtil.java
@@ -0,0 +1,32 @@
+package ui;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+/**
+ * Created by chenyuyan on 11/3/17.
+ * 异常弹窗
+ */
+public class AlertUtil {
+    public static void showInformationAlert(String text){
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText(text);
+        alert.show();
+    }
+    public static void showConfirmingAlert(String text){
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(text);
+        alert.show();
+    }
+    public static void showWarningAlert(String text){
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setContentText(text);
+        alert.show();
+    }
+
+    public static void showErrorAlert(String text){
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setContentText(text);
+        alert.show();
+    }
+}
