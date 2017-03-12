39b50bb669d7b47165f4e09ea7610a36e749fdcf
iriswuyuhan <1426546185@qq.com>
2017-03-12 15:05:00 +0800

    socket(not completed yet)

--- server/src/main/java/quantour/bl/ClientNodeThread.java
+++ server/src/main/java/quantour/bl/ClientNodeThread.java
@@ -1,18 +1,15 @@
 package quantour.bl;
 
 import java.net.Socket;
-import java.util.Stack;
 
 /**
  * Created by dell on 2017/3/11.
  */
 public class ClientNodeThread implements Runnable {
     private Socket socket;
-    private Stack<Socket> dataServerStack;
 
-    ClientNodeThread(Socket socket, Stack<Socket> dataServerStack){
+    ClientNodeThread(Socket socket){
         this.socket=socket;
-        this.dataServerStack=dataServerStack;
     }
 
     @Override
