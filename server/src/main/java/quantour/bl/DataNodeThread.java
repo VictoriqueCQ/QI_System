88099a220519c079bf02c8b41c95023ae501cdde
iriswuyuhan <1426546185@qq.com>
2017-03-12 15:05:00 +0800

    socket(not completed yet)

--- server/src/main/java/quantour/bl/DataNodeThread.java
+++ server/src/main/java/quantour/bl/DataNodeThread.java
@@ -1,11 +1,20 @@
 package quantour.bl;
 
+import java.net.Socket;
+
 /**
  * Created by dell on 2017/3/11.
  */
 public class DataNodeThread implements Runnable{
+    private Socket socket;
+
+    DataNodeThread(Socket socket){
+        this.socket=socket;
+    }
+
+
     @Override
     public void run() {
-
+        
     }
 }
