add802e9ec5becb356410ba2248de76562c58ac8
151250171 <151250171@smail.nju.edu.cn>
2017-03-13 23:28:29 +0800

    json转类

--- presentation/src/main/java/ui/JsonUtil.java
+++ presentation/src/main/java/ui/JsonUtil.java
@@ -3,6 +3,9 @@ package ui;
 import net.sf.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JsonUtil {
 
@@ -12,8 +15,12 @@ public class JsonUtil {
 
 	}
 	public  <T> Object JSONToObj(String jsonStr, Class<T> obj) {
+		jsonStr=jsonStr.substring(1,jsonStr.length()-2);
+//		System.out.print(jsonStr);
 		JSONObject jsonResult =JSONObject.fromObject(jsonStr);
-		T t=(T)JSONObject.toBean(jsonResult,obj);
+		Map<String, Class> classMap = new HashMap<String, Class>();
+		classMap.put("dates", Date.class);
+		T t=(T)JSONObject.toBean(jsonResult,obj,classMap);
 		return t;
 	}
 
