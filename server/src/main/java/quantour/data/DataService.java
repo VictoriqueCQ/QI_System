33fd6a4dac23084c50dd21db2da0b5925fc6493a
151250162 <151250162@smail.nju.edu.cn>
2017-03-13 20:53:04 +0800

    debug

--- server/src/main/java/quantour/data/DataService.java
+++ server/src/main/java/quantour/data/DataService.java
@@ -1,20 +1,18 @@
 package quantour.data;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.text.ParseException;
 
 /**
  * Created by dell on 2017/3/12.
  */
 public class DataService {
     private Socket socket;
-    private BufferedReader bufferedReader;
-    private PrintWriter printWriter;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
 
     public static void main(String[] args) {
         DataService dataService = new DataService();
@@ -30,8 +28,8 @@ public class DataService {
     private void setUpNet() {
         try {
             socket = new Socket("127.0.0.1", 9001);
-            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            printWriter = new PrintWriter(socket.getOutputStream());
+            dataInputStream=new DataInputStream(socket.getInputStream());
+            dataOutputStream=new DataOutputStream(socket.getOutputStream());
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -41,17 +39,16 @@ public class DataService {
 
 
     private void run() {
-        while (socket.isConnected()) {
-            String quest = null;
-            try {
-                if ((quest = bufferedReader.readLine()) != null) {
-                    (new ResultSender(quest, printWriter)).run();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
+        try {
+            while (socket.isConnected()) {
+                System.out.println("serverok");
+                String quest = "";
+                quest = dataInputStream.readUTF();
+                System.out.println(quest);
+                (new ResultSender(quest,dataOutputStream)).run();
             }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
