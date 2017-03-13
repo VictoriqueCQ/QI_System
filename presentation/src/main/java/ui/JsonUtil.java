2e884a13b92d83299761e25c0b9b7b9ba9354e16
151250171 <151250171@smail.nju.edu.cn>
2017-03-13 18:13:58 +0800

    界面展示结构

--- presentation/src/main/java/ui/JsonUtil.java
+++ presentation/src/main/java/ui/JsonUtil.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 
 public class JsonUtil {
 
-	public static <T> JSONObject objectToJson(T obj) throws IOException {
+	public  <T> JSONObject objectToJson(T obj) throws IOException {
 	JSONObject jsonStu = JSONObject.fromObject(obj);
 	return jsonStu;
 
 	}
-	public static <T> Object JSONToObj(String jsonStr, Class<T> obj) {
+	public  <T> Object JSONToObj(String jsonStr, Class<T> obj) {
 		JSONObject jsonResult =JSONObject.fromObject(jsonStr);
 		T t=(T)JSONObject.toBean(jsonResult,obj);
 		return t;
