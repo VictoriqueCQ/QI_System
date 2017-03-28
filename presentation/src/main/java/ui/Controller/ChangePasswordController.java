73ba30aeec6e41fa20f6cc8028b1687ceddeae66
151250171 <151250171@smail.nju.edu.cn>
2017-03-28 11:21:04 +0800

    修改密码成功2.0

--- presentation/src/main/java/ui/Controller/ChangePasswordController.java
+++ presentation/src/main/java/ui/Controller/ChangePasswordController.java
@@ -16,7 +16,7 @@ public class ChangePasswordController {
     private Net net;
 
     @FXML
-    private TextField usernameTextField;
+    private TextField IDTextField;
 
     @FXML
     private TextField passwordTextField1;
@@ -31,10 +31,10 @@ public class ChangePasswordController {
 
     @FXML
     private void changePassword(){
-        String username=usernameTextField.getText();
+        String id=IDTextField.getText();
         String oldPassword=passwordTextField1.getText();
         String newPassword=passwordTextField2.getText();
-        if(username.isEmpty()){
+        if(id.isEmpty()){
             this.exitChangePassword();
             AlertUtil.showWarningAlert("对不起，您未输入用户名");
         }
@@ -47,16 +47,21 @@ public class ChangePasswordController {
             AlertUtil.showWarningAlert("对不起，您未输入新密码");
         }
         else{
-            String input="USER\tMODIFY\t"+usernameTextField.getText()+"\t"+"NULL\t"+newPassword;
+            String input="USER\tLOGIN\t"+id+"\t"+"NULL\t"+oldPassword;
             net.actionPerformed(input);
             String json = net.run();
             JsonUtil jsonUtil = new JsonUtil();
-            UserVO userVO1=new UserVO();
-            UserVO userVO= (UserVO) jsonUtil.JSONToObj(json, userVO1.getClass());
-            if(userVO!=null){
+            UserVO userVO0=new UserVO();
+            UserVO userVO= (UserVO) jsonUtil.JSONToObj(json, userVO0.getClass());
+            System.out.print(userVO.getName());
+            String input1="USER\tMODIFY\t"+id+"\t"+userVO.getName()+"\t"+newPassword;
+            net.actionPerformed(input1);
+            String json1 = net.run();
+            UserVO userVO1= (UserVO) jsonUtil.JSONToObj(json1, userVO0.getClass());
+            if(userVO1!=null){
                 AlertUtil.showConfirmingAlert("更改密码成功");
                 this.exitChangePassword();
-                main.gotoClientOverview(true,userVO);
+                main.gotoClientOverview(true,userVO1);
             }
             else{
                 this.exitChangePassword();
