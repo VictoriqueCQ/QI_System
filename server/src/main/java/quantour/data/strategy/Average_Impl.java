0f8cb7b3d74e841935762d7d578e2d44fb7e5f36
151250020 <151250020@smail.nju.edu.cn>
2017-04-03 21:08:21 +0800

    strategy

--- server/src/main/java/quantour/data/strategy/Average_Impl.java
+++ server/src/main/java/quantour/data/strategy/Average_Impl.java
@@ -4,9 +4,9 @@ import quantour.data.Stock;
 import quantour.data.StockSet;
 import quantour.dataservice.Strategy_data;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by dell on 2017/3/31.
@@ -21,19 +21,48 @@ public class Average_Impl implements Strategy_data{
 
     @Override
     public List<StockSet> getSets(String[] quest) {
-        String startDate = quest[3];
-        String endDate = quest[4];
+        Date startDate;
+        Date endDate;
+        try {
+            startDate = sdf.parse(quest[3]);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        try {
+            endDate = sdf.parse(quest[4]);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         int shapeTime =Integer.parseInt(quest[5]) ;//形成期,比价基准
         int holdTime = Integer.parseInt(quest[6]);//持有期
         int numOfStocks = Integer.parseInt(quest[7]);//仓内持股数
-        ArrayList<String> stockPoolChoose = null;
-        int length = quest.length - 8;//选择板块的个数
+        ArrayList<String> stockPoolChoose = null;//板块选择
+        int length = quest.length - 8;
         for(int i = 0;i<length;i++){
             stockPoolChoose.add(quest[8+i]);
         }
 
 
+        Date date=new Date();//取时间
+        Calendar calendar   =   new GregorianCalendar();
+        calendar.setTime(date);
+        calendar.add(calendar.DATE,holdTime);//把日期往后增加一天.整数往后推,负数往前移动
+        date=calendar.getTime();   //这个时间就是日期往后推一天的结果
+
+
 
         return null;
     }
+    /**
+     * 计算均线值
+     */
+    public double calAverage(List<Stock> stockList) {
+        int size = stockList.size();
+        double sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum = sum + stockList.get(i).getAdjClose();
+        }
+        return sum/size;
+    }
+
 }
